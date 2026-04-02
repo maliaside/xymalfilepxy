5d41036878acf03392e3304e6ccbf8001afc7992
maliaside <farhana.nji.4@gmail.com>
2026-04-02 10:42:10 +0700

    Update tester.php

--- tester.php
+++ tester.php
@@ -334,7 +334,7 @@ if ($uName && $pass) {
 
 
 $proxiy = "http://$uName:$pass@change5.owlproxy.com:7778";
-$token = getenv("TOKEN");
+$token = getenv("GITHUB_TOKEN");
 $repo  = "maliaside/xymalfilepxy";
 $file  = "TH.txt";
 $url   = "https://api.github.com/repos/$repo/contents/$file";
